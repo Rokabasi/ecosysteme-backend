e1e3be2c5f98ce5792856e213250b5b2516e7e59
Bia nzambi <kabasiromain@gmail.com>
2025-10-16 22:42:29 +0100

    main:fix cors

--- app.js
+++ app.js
@@ -25,7 +25,50 @@ const dashboardRouter = require('./routes/dashboard');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(cors());
+// Configuration CORS détaillée pour permettre les requêtes entre sous-domaines
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Autoriser les requêtes sans origine (comme les appels API directs)
+    if (!origin) return callback(null, true);
+
+    // Liste des domaines autorisés
+    const allowedOrigins = [
+      "https://fonarev.mazaya.io",
+      "https://public.mazaya.io",
+      "https://ecoapi.mazaya.io/",
+      "http://46.202.194.100:3011",
+      "http://46.202.194.100:3010",
+      "http://46.202.194.100:3014",
+      "http://localhost:5173",
+      "http://localhost:5174",
+      // Ajoutez d'autres domaines si nécessaire
+    ];
+
+    if (allowedOrigins.indexOf(origin) !== -1 || origin.endsWith("mazaya.io")) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: [
+    "Origin",
+    "X-Requested-With",
+    "Content-Type",
+    "Accept",
+    "Authorization",
+    "x-access-token",
+  ],
+  credentials: true, // Pour permettre les cookies dans les requêtes cross-origin
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+
+// Appliquer CORS globalement
+app.use(cors(corsOptions));
+
+// Assurer que OPTIONS répond correctement pour les requêtes préflight
+app.options("*", cors(corsOptions));
 
 app.use(logger('dev'));
 app.use(express.json());
